3099cb95fcaacc76ad46b230c769dc943f6a9eea
roironen <RONEN.ROI@GMAIL.COM>
2023-04-19 14:14:51 +0300

    support multi-datasets training

--- VIPCT/dataloader/dataset.py
+++ VIPCT/dataloader/dataset.py
@@ -68,89 +68,123 @@ def get_cloud_datasets(
         train_dataset: The training dataset object.
         val_dataset: The validation dataset object.
     """
-    dataset_name = cfg.data.dataset_name
-
-    if dataset_name not in ALL_DATASETS:
-        raise ValueError(f"'{dataset_name}'' does not refer to a known dataset.")
-
-    if dataset_name == 'CASS_10cameras_20m':
-        data_root = os.path.join(data_root, 'CASS_50m_256x256x139_600CCN/10cameras_50m')
-        image_size = [236, 236]
-    elif dataset_name == 'BOMEX_CASS_10cameras_20m':
-        data_root_cass = os.path.join(data_root, 'CASS_50m_256x256x139_600CCN/10cameras_20m')
-        data_root_bomex = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m')
-    elif dataset_name == 'CASS_10cameras_50m':
-        data_root = os.path.join(data_root, 'CASS_50m_256x256x139_600CCN/10cameras_50m')
-        image_size = [96, 96]
-    elif dataset_name == 'BOMEX_10cameras_20m':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_10cameras_20m_varying_S':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_S')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_10cameras_20m_varying_M':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_M')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_10cameras_20m_varying_L':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_L')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_10cameras_20m_varying_XL':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_XL')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_10cameras_50m':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_50m')
-        image_size = [48, 48]
-    elif dataset_name == 'BOMEX_32cameras_50m':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '32cameras_50m')
-        image_size = [48, 48]
-    elif dataset_name == 'BOMEX_32cameras':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '32cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'subset_of_seven_clouds':
-        data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_50m')
-        image_size = [48, 48]
-
-    elif dataset_name == 'BOMEX_50CCN_10cameras_20m':
-        data_root = os.path.join(data_root, 'BOMEX_128x128x100_50CCN_50m_micro_256', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_50CCN_aux_10cameras_20m':
-        data_root = os.path.join(data_root, 'BOMEX_128x128x100_50CCN_50m_micro_256_aux4', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'BOMEX_5000CCN_new_10cameras_20m':
-        data_root = os.path.join(data_root, 'BOMEX_128x128x100_5000CCN_50m_micro_256', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'CASS_600CCN_roiprocess_10cameras_20m':
-        data_root = os.path.join(data_root, 'CASS_256x256x139_600CCN_50m_32x32x32_roipreprocess', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'HAWAII_2000CCN_10cameras_20m':
-        data_root = os.path.join(data_root, 'HAWAII_2000CCN_32x32x64_50m', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'DYCOMS_RF02_50CCN_10cameras_20m':
-        data_root = os.path.join(data_root, 'DYCOMS_RF02_50CCN_64x64x159_50m', '10cameras_20m')
-        image_size = [236, 236]
-    elif dataset_name == 'DYCOMS_RF02_500CCN_10cameras_20m':
-        data_root = os.path.join(data_root, 'DYCOMS_RF02_500CCN_64x64x159_50m', '10cameras_20m')
-        image_size = [236, 236]
-
-    elif dataset_name == 'Toy_10cameras_20m':
-        data_root = os.path.join(data_root, 'Toy', '10cameras_20m')
-        image_size = [116, 116]
-    elif dataset_name == 'Toy2_10cameras_20m':
-        data_root = os.path.join(data_root, 'Toy2', '10cameras_20m')
-        image_size = [116, 116]
+    dataset_names = cfg.data.dataset_name
+    if isinstance(dataset_names,str):
+        dataset_names = [dataset_names]
     else:
-        FileNotFoundError()
+        dataset_names = list(dataset_names)
+    train_paths=[]
+    val_paths = []
+    for dataset_name in dataset_names:
+        if dataset_name not in ALL_DATASETS:
+            raise ValueError(f"'{dataset_name}'' does not refer to a known dataset.")
 
-    if not dataset_name == 'BOMEX_CASS_10cameras_20m':
-        print(f"Loading dataset {dataset_name}, image size={str(image_size)} ...")
-        data_train_paths = [f for f in glob.glob(os.path.join(data_root, "train/cloud*.pkl"))]
-    else:
-        print(f"Loading dataset {dataset_name}...")
-        data_train_paths = [f for f in glob.glob(os.path.join(data_root_cass, "train/cloud*.pkl"))]
-        data_train_paths += [f for f in glob.glob(os.path.join(data_root_bomex, "train/cloud*.pkl"))]
-        random.shuffle(data_train_paths)
-    train_len = cfg.data.n_training if cfg.data.n_training>0 else len(data_train_paths)
-    data_train_paths = data_train_paths[:train_len]
+        if dataset_name == 'CASS_10cameras_20m':
+            data_root = os.path.join(data_root, 'CASS_50m_256x256x139_600CCN/10cameras_50m')
+            image_size = [236, 236]
+        elif dataset_name == 'BOMEX_CASS_10cameras_20m':
+            data_root_cass = os.path.join(data_root, 'CASS_50m_256x256x139_600CCN/10cameras_20m')
+            data_root_bomex = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m')
+        elif dataset_name == 'CASS_10cameras_50m':
+            data_root = os.path.join(data_root, 'CASS_50m_256x256x139_600CCN/10cameras_50m')
+            image_size = [96, 96]
+        elif dataset_name == 'BOMEX_10cameras_20m':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_10cameras_20m_varying_S':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_S')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_10cameras_20m_varying_M':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_M')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_10cameras_20m_varying_L':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_L')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_10cameras_20m_varying_XL':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_20m_varying_XL')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_10cameras_50m':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_50m')
+            image_size = [48, 48]
+        elif dataset_name == 'BOMEX_32cameras_50m':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '32cameras_50m')
+            image_size = [48, 48]
+        elif dataset_name == 'BOMEX_32cameras':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '32cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'subset_of_seven_clouds':
+            data_root = os.path.join(data_root, 'BOMEX_256x256x100_5000CCN_50m_micro_256', '10cameras_50m')
+            image_size = [48, 48]
+
+        elif dataset_name == 'BOMEX_50CCN_10cameras_20m':
+            data_root = os.path.join(data_root, 'BOMEX_128x128x100_50CCN_50m_micro_256', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_50CCN_aux_10cameras_20m':
+            data_root = os.path.join(data_root, 'BOMEX_128x128x100_50CCN_50m_micro_256_aux4', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'BOMEX_5000CCN_new_10cameras_20m':
+            data_root = os.path.join(data_root, 'BOMEX_128x128x100_5000CCN_50m_micro_256', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'CASS_600CCN_roiprocess_10cameras_20m':
+            data_root = os.path.join(data_root, 'CASS_256x256x139_600CCN_50m_32x32x32_roipreprocess', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'HAWAII_2000CCN_10cameras_20m':
+            data_root = os.path.join(data_root, 'HAWAII_2000CCN_32x32x64_50m', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'DYCOMS_RF02_50CCN_10cameras_20m':
+            data_root = os.path.join(data_root, 'DYCOMS_RF02_50CCN_64x64x159_50m', '10cameras_20m')
+            image_size = [236, 236]
+        elif dataset_name == 'DYCOMS_RF02_500CCN_10cameras_20m':
+            data_root = os.path.join(data_root, 'DYCOMS_RF02_500CCN_64x64x159_50m', '10cameras_20m')
+            image_size = [236, 236]
+
+        elif dataset_name == 'Toy_10cameras_20m':
+            data_root = os.path.join(data_root, 'Toy', '10cameras_20m')
+            image_size = [116, 116]
+        elif dataset_name == 'Toy2_10cameras_20m':
+            data_root = os.path.join(data_root, 'Toy2', '10cameras_20m')
+            image_size = [116, 116]
+        else:
+            FileNotFoundError()
+
+        if not dataset_name == 'BOMEX_CASS_10cameras_20m':
+            print(f"Loading dataset {dataset_name}, image size={str(image_size)} ...")
+            data_train_path = [f for f in glob.glob(os.path.join(data_root, "train/cloud*.pkl"))]
+            train_len = cfg.data.n_training if cfg.data.n_training > 0 else len(data_train_path)
+            train_paths += data_train_path[:train_len]
+        else:
+            print(f"Loading dataset {dataset_name}...")
+            data_train_path1 = [f for f in glob.glob(os.path.join(data_root_cass, "train/cloud*.pkl"))]
+            train_len = cfg.data.n_training if cfg.data.n_training > 0 else len(data_train_path1)
+            data_train_paths = data_train_path1[:train_len]
+            data_train_path2 = [f for f in glob.glob(os.path.join(data_root_bomex, "train/cloud*.pkl"))]
+            train_len = cfg.data.n_training if cfg.data.n_training > 0 else len(data_train_path2)
+            data_train_paths += data_train_path1[:train_len]
+
+        if not (dataset_name == 'BOMEX_CASS_10cameras_20m' or dataset_name == 'subset_of_seven_clouds'):
+            val_path = [f for f in glob.glob(os.path.join(data_root, "test/cloud*.pkl"))]
+            val_len = cfg.data.n_val if cfg.data.n_val > 0 else len(val_path)
+            val_paths += val_path[:val_len]
+        elif dataset_name == 'subset_of_seven_clouds':
+            val_path = [f for f in glob.glob(os.path.join(data_root, "subset_of_seven_clouds/cloud*.pkl"))]
+            val_paths += val_path
+        else:
+            val_path = [f for f in glob.glob(os.path.join(data_root_cass, "test/cloud*.pkl"))]
+            val_len = cfg.data.n_val if cfg.data.n_val > 0 else len(val_path)
+            val_paths += val_path[:val_len]
+
+            val_path = [f for f in glob.glob(os.path.join(data_root_bomex, "test/cloud*.pkl"))]
+            val_len = cfg.data.n_val if cfg.data.n_val > 0 else len(val_path)
+            val_paths += val_path[:val_len]
+
+
+
+
+        # val_paths = val_paths[:val_len]
+    random.shuffle(train_paths)
+    random.shuffle(val_paths)
+
+    # data_train_paths = data_train_paths[:train_len]
 
     n_cam = cfg.data.n_cam
     mean = cfg.data.mean
@@ -158,28 +192,17 @@ def get_cloud_datasets(
     rand_cam = cfg.data.rand_cam
 
     train_dataset = CloudDataset(
-            data_train_paths,
+            train_paths,
         n_cam=n_cam,
         rand_cam = rand_cam,
         mask_type=cfg.ct_net.mask_type,
         mean=mean,
         std=std,
-    dataset_name = dataset_name,
+    dataset_name = dataset_names,
         noise=cfg.data.noise
 
     )
-    if not (dataset_name == 'BOMEX_CASS_10cameras_20m' or dataset_name == 'subset_of_seven_clouds'):
-        val_paths = [f for f in glob.glob(os.path.join(data_root, "test/cloud*.pkl"))]
-    elif dataset_name == 'subset_of_seven_clouds':
-        val_paths = [f for f in glob.glob(os.path.join(data_root, "subset_of_seven_clouds/cloud*.pkl"))]
-        print(val_paths)
-    else:
-        val_paths = [f for f in glob.glob(os.path.join(data_root_cass, "test/cloud*.pkl"))]
-        val_paths += [f for f in glob.glob(os.path.join(data_root_bomex, "test/cloud*.pkl"))]
-        random.shuffle(val_paths)
 
-    val_len = cfg.data.n_val if cfg.data.n_val>0 else len(val_paths)
-    val_paths = val_paths[:val_len]
     val_dataset = CloudDataset(val_paths, n_cam=n_cam,
         rand_cam = rand_cam, mask_type=cfg.ct_net.val_mask_type, mean=mean, std=std,   dataset_name = dataset_name,
         noise=cfg.data.noise
@@ -208,7 +231,6 @@ class CloudDataset(Dataset):
 
     def __getitem__(self, idx):
         cloud_path = self.cloud_dir[idx]
-
         with open(cloud_path, 'rb') as f:
             data = pickle.load(f)
         images = data['images']
